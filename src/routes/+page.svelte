977773c6b6783e461703653b5bbcc7b4312e0650
Frederik Shull <zlyfer@pm.me>
2023-05-10 21:16:55 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -119,7 +119,7 @@
 		</div>
 		<div class="info">
 			<span class="text">
-				Hello there :D
+				Hello there,
 				<br />
 				<code>Frederik Shull</code> aka <code>zlyfer</code> here!
 				<br />
