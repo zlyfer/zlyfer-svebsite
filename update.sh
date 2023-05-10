1b3a8cad194ce036ea070cfeded5b7df430866d8
Frederik Shull <zlyfer@zlyfer.net>
2023-05-10 22:39:06 +0200

    update fix

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/zlyfer/websites/zlyfer-svebsite
 git pull
+#/home/zlyfer/.nvm/versions/node/v16.14.2/bin/npm install
+#/home/zlyfer/.nvm/versions/node/v16.14.2/bin/npm run build
+nvm use 16
 npm install
 npm run build
 systemctl --user restart svebsite
