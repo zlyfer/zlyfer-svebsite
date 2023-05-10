e737b7e1d86a903b24d41c22f33e491640cec1c2
Frederik Shull <zlyfer@pm.me>
2023-05-10 20:17:44 +0200

    test

--- update.js
+++ update.js
@@ -2,35 +2,37 @@ const http = require('http');
 const { exec } = require('child_process');
 const secret = '2ShCW82rZUmz&CBBgsc$$bBmZ%wxR656';
 
-http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
+http
+	.createServer((req, res) => {
+		if (req.method === 'POST') {
+			let body = '';
 
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
+			req.on('data', (chunk) => {
+				body += chunk.toString();
+			});
 
-    req.on('end', () => {
-      const signature = req.headers['x-hub-signature-256'];
+			req.on('end', () => {
+				const signature = req.headers['x-hub-signature-256'];
+				console.log(signature);
 
-      if (signature && signature === secret) {
-        // Execute the update script
-        exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.error(`stderr: ${stderr}`);
-        });
-      }
+				if (signature && signature === secret) {
+					// Execute the update script
+					exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
+						if (error) {
+							console.error(`exec error: ${error}`);
+							return;
+						}
+						console.log(`stdout: ${stdout}`);
+						console.error(`stderr: ${stderr}`);
+					});
+				}
 
-      res.writeHead(200);
-      res.end();
-    });
-  } else {
-    res.writeHead(200);
-    res.end();
-  }
-}).listen(9000);
-
+				res.writeHead(200);
+				res.end();
+			});
+		} else {
+			res.writeHead(200);
+			res.end();
+		}
+	})
+	.listen(9000);
