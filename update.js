6a23e1f1ab7c44b600078c6636c2144bd9690eb8
Frederik Shull <zlyfer@pm.me>
2023-05-10 21:10:34 +0200

    fixed webhook

--- update.js
+++ update.js
@@ -6,25 +6,25 @@ console.log('Starting webhook.');
 http
 	.createServer((req, res) => {
 		if (req.method === 'POST') {
-			// let body = '';
+			let body = '';
 
-			// req.on('data', (chunk) => {
-			// 	body += chunk.toString();
-			// });
+			req.on('data', (chunk) => {
+				body += chunk.toString();
+			});
 
 			req.on('end', () => {
 				// const signature = req.headers['x-hub-signature-256'];
 
 				console.log('New POST request at: ' + new Date().toLocaleString());
 
-				exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
-					if (error) {
-						console.error(`exec error: ${error}`);
-						return;
-					}
-					console.log(`stdout: ${stdout}`);
-					console.error(`stderr: ${stderr}`);
-				});
+				// exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
+				// 	if (error) {
+				// 		console.error(`exec error: ${error}`);
+				// 		return;
+				// 	}
+				// 	console.log(`stdout: ${stdout}`);
+				// 	console.error(`stderr: ${stderr}`);
+				// });
 
 				res.writeHead(200);
 				res.end();
