e777ff2d88f1516d8026c53f8a81b586be2c869a
Frederik Shull <zlyfer@pm.me>
2023-05-10 20:45:16 +0200

    test

--- update.js
+++ update.js
@@ -1,31 +1,32 @@
 import http from 'http';
 import { exec } from 'child_process';
-const { secret } = './update-signature.jsonc';
 
 http
 	.createServer((req, res) => {
 		if (req.method === 'POST') {
-			let body = '';
+			console.log(
+				'New POST request at: ' +
+					new Date().toLocaleString() +
+					' from: ' +
+					req.connection.remoteAddress
+			);
+			// let body = '';
 
-			req.on('data', (chunk) => {
-				body += chunk.toString();
-				console.log(body);
-			});
+			// req.on('data', (chunk) => {
+			// 	body += chunk.toString();
+			// });
 
 			req.on('end', () => {
-				const signature = req.headers['x-hub-signature-256'];
+				// const signature = req.headers['x-hub-signature-256'];
 
-				if (signature && signature === 'sha256=' + secret) {
-					console.log('Signature valid.');
-					exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
-						if (error) {
-							console.error(`exec error: ${error}`);
-							return;
-						}
-						console.log(`stdout: ${stdout}`);
-						console.error(`stderr: ${stderr}`);
-					});
-				}
+				exec('bash /home/zlyfer/websites/zlyfer-svebsite/update.sh', (error, stdout, stderr) => {
+					if (error) {
+						console.error(`exec error: ${error}`);
+						return;
+					}
+					console.log(`stdout: ${stdout}`);
+					console.error(`stderr: ${stderr}`);
+				});
 
 				res.writeHead(200);
 				res.end();
