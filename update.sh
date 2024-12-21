2704a59c6eadabc9866c4cddd9b2df49e10dcb20
Frederik Shull <zlyfer@pm.me>
2024-12-22 00:31:21 +0100

    adjust

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-source /home/zlyfer/.nvm/nvm.sh
+#source /home/zlyfer/.nvm/nvm.sh
 
 cd /var/www/html/net.zlyfer
 git pull
-nvm use 16.14
-npm install
-npm run build
-systemctl restart svebsite
-
+#nvm use 16.14
+/home/zlyfer/.local/share/fnm/node-versions/v16.16.0/installation/bin/npm install
+/home/zlyfer/.local/share/fnm/node-versions/v16.16.0/installation/bin/npm run build
+sudo systemctl restart svebsite
